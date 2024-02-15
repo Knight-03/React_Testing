c445c8b64d56db437d00cf522391614cfd70fef5
Knight-03 <drp9975@gmail.com>
2024-02-15 22:58:46 +0530

    added types of test and Jest Watch Mode

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,9 +1,22 @@
-import { render, screen } from "@testing-library/react"
-import { Greet } from "./greet"
+/*
+* Greet should render the text hello and if a name is passed into the component then
+* It shold render  hello followed by the name
+*/
 
-// First Test
-test("greet renders correctly", () => {
+import { render, screen } from "@testing-library/react";
+import { Greet } from "./greet";
+
+test("Greet render correctly", () => {
     render(<Greet/>)
-    const textElement = screen.getByText(/hello/i)
+    const textElement = screen.getByText("Hello");
     expect(textElement).toBeInTheDocument()
 })
+
+test("Greet render with name", () => {
+    render(<Greet name='Dhvani'/>)
+    const textElement = screen.getByText("Hello Dhvani");
+    expect(textElement).toBeInTheDocument()
+})
+
+// test.only will execute only that test 
+// test.skip will skip that test
