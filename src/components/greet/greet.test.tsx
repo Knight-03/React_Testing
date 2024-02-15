34b3920d9358914aad04271b4867bf4ee3b5ca52
Knight-03 <drp9975@gmail.com>
2024-02-15 23:21:34 +0530

    how to group tests , can nest the group, can make multiple group

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,22 +1,38 @@
 /*
-* Greet should render the text hello and if a name is passed into the component then
-* It shold render  hello followed by the name
-*/
+ * Greet should render the text hello and if a name is passed into the component then
+ * It shold render  hello followed by the name
+ */
 
 import { render, screen } from "@testing-library/react";
 import { Greet } from "./greet";
 
-test("Greet render correctly", () => {
-    render(<Greet/>)
+describe("Greet", () => {
+  test("render correctly", () => {
+    render(<Greet />);
     const textElement = screen.getByText("Hello");
-    expect(textElement).toBeInTheDocument()
-})
+    expect(textElement).toBeInTheDocument();
+  });
 
-test("Greet render with name", () => {
-    render(<Greet name='Dhvani'/>)
-    const textElement = screen.getByText("Hello Dhvani");
-    expect(textElement).toBeInTheDocument()
-})
+    // /nested describe blocks
+  describe("Nested", () => {
+    test("render with name", () => {
+      render(<Greet name="Dhvani" />);
+      const textElement = screen.getByText("Hello Dhvani");
+      expect(textElement).toBeInTheDocument();
+    });
+  });
+});
 
-// test.only will execute only that test 
+
+// multiple describe blocks 
+describe("Multiple", () => {
+    test("render with name", () => {
+      render(<Greet name="Patel" />);
+      const textElement = screen.getByText("Hello Patel");
+      expect(textElement).toBeInTheDocument();
+    });
+  });
+// test.only will execute only that test
 // test.skip will skip that test
+
+//* In terminal Test Suits corresponds to a individual files not an individual "describe block"
